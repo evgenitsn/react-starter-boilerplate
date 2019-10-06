36f926edb49fc068725f385514188de74c32f326
Evgeni Nikolov <evgeni.tsn@gmail.com>
2019-10-06 21:21:49 +0300

    feat(src): added React router with two basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import { Home, About } from './screens'
 
 function App() {
-  return <div>Hello World</div>
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+      </div>
+    </Router>
+  )
 }
 
 export default App
