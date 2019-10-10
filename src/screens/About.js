0ed460d6b70291fcc2e4ff729b340b6438bcbb48
Evgeni Nikolov <evgeni.tsn@gmail.com>
2019-10-10 19:04:22 +0300

    feat: added forms support

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import useForm from 'react-hook-form'
 
 function About() {
-  return <div>About</div>
+  const { register, errors, handleSubmit } = useForm()
+  const onSubmit = data => {
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <h2>About</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name='firstName' ref={register} />
+        <input name='lastName' ref={register({ required: true })} />
+        {errors.lastName && <span>This field is required</span>}
+        <input type='submit' />
+      </form>
+    </div>
+  )
 }
 
 export default About
